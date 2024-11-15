976adda008115caa12bdf480f1a95714ac700f1a
Alex Su <suziyue@gmail.com>
2024-11-14 16:42:25 -0800

    Changed deque to linked list

--- project/deque.c
+++ project/deque.c
@@ -1,110 +1,135 @@
 #include <stdio.h>
 #include "deque.h"
 
-struct queue_t {
-    packet *queue;
-    uint8_t front;
-    uint8_t back;
-    uint8_t size;
-    uint8_t capacity;
+typedef struct queue_node_t node;
+
+struct queue_node_t {
+    packet* p;
+    node* next;
+    node* prev;
 };
 
-static uint8_t increment(q_handle_t self, uint8_t idx) {
-    if (idx == self->capacity - 1)
-        return 0;
-    else
-        return ++idx;
-}
+struct queue_t {
+    node* dummy_head;
+    uint32_t size;
+    uint32_t capacity;
+};
 
-static uint8_t decrement(q_handle_t self, uint8_t idx) {
-    if (idx == 0)
-        return self->capacity - 1;
-    else
-        return --idx;
-}
-
-q_handle_t q_init(uint8_t capacity) {
-    q_handle_t self = malloc(sizeof(packet*) + 4);
+q_handle_t q_init(uint32_t capacity) {
+    q_handle_t self = malloc(sizeof(struct queue_t));
     if (self != NULL) {
-        self->back = 0;
-        self->front = 0;
+        self->dummy_head = malloc(sizeof(node));
         self->size = 0;
         self->capacity = capacity;
-        self->queue = malloc(sizeof(packet) * capacity);
-        if (self->queue == NULL) {
+        if (self->dummy_head == NULL) {
             free(self);
             self = NULL;
+        } else {
+            self->dummy_head->p = NULL;
+            self->dummy_head->next = self->dummy_head;
+            self->dummy_head->prev = self->dummy_head;
         }
     }
     return self;
 }
 
 void q_destroy(q_handle_t self) {
-    free(self->queue);
+    q_clear(self);
+    free(self->dummy_head);
     free(self);
 }
 
 void q_clear(q_handle_t self) {
-    self->front = 0;
-    self->back = 0;
+    for (node* curr = self->dummy_head->next; curr != self->dummy_head;) {
+        node* next = curr->next;
+        free(curr->p);
+        free(curr);
+        curr = next;
+    }
+    self->dummy_head->next = self->dummy_head;
+    self->dummy_head->prev = self->dummy_head;
     self->size = 0;
 }
 
-bool q_push_back(q_handle_t self, packet *pkt) {
-    if (q_full(self)) return false;
-    self->queue[self->back] = *pkt;
-    self->back = increment(self, self->back);
-    self->size++;
-    return true;
-}
-
-bool q_push_front(q_handle_t self, packet *pkt) {
-    if (q_full(self)) return false;
-    self->front = decrement(self, self->front);
-    self->queue[self->front] = *pkt;
-    self->size++;
-    return true;
-}
-
-bool q_try_insert_keep_sorted(q_handle_t self, packet *pkt) {
-    if (q_full(self)) return false;
-    // Prevent duplicates
-    for (uint8_t i = self->front; i != self->back; i = increment(self, i)) {
-        if (self->queue[i].seq == pkt->seq) return false;
-    }
-    self->front = decrement(self, self->front);
-    uint8_t curr = self->front;
-    for (uint8_t i = 0; i < self->size; curr = increment(self, curr), i++) {
-        uint8_t next = increment(self, curr);
-        if (self->queue[next].seq > pkt->seq) break;
-        self->queue[curr] = self->queue[next];
-    }
-    self->size++;
-    self->queue[curr] = *pkt;
-    return true;
-}
-
-packet* q_pop_front(q_handle_t self) {
-    if (q_empty(self)) {
+static node* make_node(const packet *pkt) {
+    node* new_node = malloc(sizeof(node));
+    if (new_node == NULL) return NULL;
+    new_node->p = malloc(sizeof(packet));
+    if (new_node->p == NULL) {
+        free(new_node);
         return NULL;
     } else {
-        packet* retval = &self->queue[self->front];
-        self->front = increment(self, self->front);
-        self->size--;
-        return retval;
+        *(new_node->p) = *pkt;
     }
+    return new_node;
+}
+
+bool q_push_back(q_handle_t self, const packet *pkt) {
+    if (q_full(self)) return false;
+    node* new_node = make_node(pkt);
+    if (new_node == NULL)
+        return false;
+    node* before = self->dummy_head->prev;
+    before->next = new_node;
+    new_node->prev = before;
+    self->dummy_head->prev = new_node;
+    new_node->next = self->dummy_head;
+    self->size++;
+    return true;
+}
+
+bool q_push_front(q_handle_t self, const packet *pkt) {
+    if (q_full(self)) return false;
+    node* new_node = make_node(pkt);
+    if (new_node == NULL)
+        return false;
+    node* after = self->dummy_head->next;
+    after->prev = new_node;
+    new_node->next = after;
+    self->dummy_head->next = new_node;
+    new_node->prev = self->dummy_head;
+    self->size++;
+    return true;
+}
+
+bool q_try_insert_keep_sorted(q_handle_t self, const packet *pkt) {
+    if (q_full(self)) return false;
+    // Prevent duplicates
+    node* curr;
+    for (curr = self->dummy_head->next; curr != self->dummy_head; curr = curr->next) {
+        if (curr->p->seq == pkt->seq) return false;
+        if (curr->p->seq > pkt->seq) break;
+    }
+    node* new_node = make_node(pkt);
+    new_node->next = curr;
+    new_node->prev = curr->prev;
+    curr->prev->next = new_node;
+    curr->prev = new_node;
+    self->size++;
+    return true;
+}
+
+bool q_pop_front(q_handle_t self, packet *pkt) {
+    if (q_empty(self))
+        return false;
+    node* target = self->dummy_head->next;
+    self->dummy_head->next = target->next;
+    target->next->prev = self->dummy_head;
+    if (pkt != NULL)
+        *pkt = *(target->p);
+    free(target->p);
+    free(target);
+    self->size--;
+    return true;
 }
 
 packet* q_pop_front_get_next(q_handle_t self) {
-    q_pop_front(self);
+    q_pop_front(self, NULL);
     return q_front(self);
 }
 
 packet* q_front(q_handle_t self) {
-    if (q_empty(self))
-        return NULL;
-    else
-        return &self->queue[self->front];
+    return self->dummy_head->next->p;
 }
 
 size_t q_size(q_handle_t self) {
@@ -112,19 +137,20 @@ size_t q_size(q_handle_t self) {
 }
 
 bool q_full(q_handle_t self) {
-    return self->size == self->capacity;
+    return self->size >= self->capacity;
 }
 
 bool q_empty(q_handle_t self) {
     return self->size == 0;
 }
 
+void q_set_capacity(q_handle_t self, uint32_t capacity) {
+    self->capacity = capacity;
+}
+
 void q_print(q_handle_t self, const char *str) {
     fprintf(stderr, "%s", str);
-    for (uint8_t i = 0, j = self->front;
-        i < self->size; i++,
-        j = increment(self, j)) {
-        fprintf(stderr, " %u", self->queue[j].seq);
-    }
+    for (node* curr = self->dummy_head->next; curr != self->dummy_head; curr = curr->next)
+        fprintf(stderr, " %u", curr->p->seq);
     fprintf(stderr, "\n");
 }
--- project/deque.h
+++ project/deque.h
@@ -7,18 +7,19 @@
 
 typedef struct queue_t* q_handle_t;
 
-q_handle_t q_init(uint8_t capacity);
+q_handle_t q_init(uint32_t capacity);
 void q_destroy(q_handle_t self);
 void q_clear(q_handle_t self);
-bool q_push_back(q_handle_t self, packet *pkt);
-bool q_push_front(q_handle_t self, packet *pkt);
-bool q_try_insert_keep_sorted(q_handle_t self, packet *pkt);
-packet* q_pop_front(q_handle_t self);
+bool q_push_back(q_handle_t self, const packet *pkt);
+bool q_push_front(q_handle_t self, const packet *pkt);
+bool q_try_insert_keep_sorted(q_handle_t self, const packet *pkt);
+bool q_pop_front(q_handle_t self, packet *pkt);
 packet* q_pop_front_get_next(q_handle_t self);
 packet* q_front(q_handle_t self);
 size_t q_size(q_handle_t self);
 bool q_full(q_handle_t self);
 bool q_empty(q_handle_t self);
+void q_set_capacity(q_handle_t self, uint32_t capacity);
 void q_print(q_handle_t self, const char *str);
 
 #endif  // PROJECT_DEQUE_H_
